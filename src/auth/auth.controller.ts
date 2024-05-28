161b92bc94c69dea53a0695cf633ef94ed19d856
katiaipduarte <katiaipduarte@hotmail.com>
2024-05-28 18:29:43 +0100

    feat: update auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Profile } from 'passport-spotify';
+import { Response } from 'express';
+import { SpotifyOauthGuard } from './guards/spotify-oauth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(SpotifyOauthGuard)
+  @Get('login')
+  login(): void {
+    return;
+  }
+
+  @UseGuards(SpotifyOauthGuard)
+  @Get('redirect')
+  async spotifyAuthRedirect(
+    @Req() req: any,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const {
+      user,
+      authInfo,
+    }: {
+      user: Profile;
+      authInfo: {
+        accessToken: string;
+        refreshToken: string;
+        expires_in: number;
+      };
+    } = req;
+
+    if (!user) {
+      res.redirect('/');
+      return;
+    }
+
+    req.user = undefined;
+
+    const jwt = this.authService.login(user);
+
+    res.set('authorization', `Bearer ${jwt}`);
+
+    return res.status(201).json({ authInfo, user });
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { SpotifyOauthStrategy } from './strategies/spotify-oauth.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: async () => {
+        return {
+          secret: process.env.JWT_SECRET,
+          signOptions: {
+            expiresIn: '3600s',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, SpotifyOauthStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
